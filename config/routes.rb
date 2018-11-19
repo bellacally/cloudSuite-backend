a65daabc368d5904242cd1b0d61986e33920cddc
Benjamin Chen <yeehenchen@gmail.com>
2018-11-19 18:43:27 +0800

    first version of routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # api format : api/v1/...
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :users, only: [:show, :update] do
+        resources :jets, only: [:create, :update, :destroy]
+      end
+      resources :jets, only: [:index, :show] do
+        resources :reservations, only: [:create]
+      end
+      resources :reservations, only: [:show, :update, :destroy] do
+        resources :reviews, only: [:create]
+      end
+    end
+  end
 end
