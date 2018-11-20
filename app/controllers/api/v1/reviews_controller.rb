a91e11e23b628bb999514a67f0f26183f0a77ee7
AveryJin <avery.jin@gmail.com>
2018-11-20 18:01:00 +0800

    added reviews controller

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::ReviewsController < Api::V1::BaseController
+   skip_before_action :verify_authenticity_token
+
+  def create
+    @reservation = Reservation.find(params[:reservation_id])
+    @review = Review.new(review_params)
+    if @review.save
+        render json: {
+          review: @review
+        }
+    else
+      render_error
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating, :photo, :reservation_id)
+  end
+
+  def render_error
+    render json: { errors: @jet.errors.full_messages },
+      status: :unprocessable_entity
+  end
+end
