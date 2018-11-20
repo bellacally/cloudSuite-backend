863338bdfcf39be458ead333625ac1dc532fb6ab
AveryJin <avery.jin@gmail.com>
2018-11-20 13:55:19 +0800

    added jets controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # api format : api/v1/...
+
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :users, only: [:show, :update]
@@ -12,4 +13,6 @@ Rails.application.routes.draw do
       end
     end
   end
+
+  root to: 'api/v1/jets#index'
 end
