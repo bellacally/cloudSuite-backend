855d5308f569c5c3b345b7ed58bb713db3b6cafb
Benjamin Chen <yeehenchen@gmail.com>
2018-11-20 10:34:53 +0800

    add openid to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_20_022659) do
+ActiveRecord::Schema.define(version: 2018_11_20_023329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2018_11_20_022659) do
     t.string "phone_number"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "openid"
   end
 
   add_foreign_key "jets", "users"
