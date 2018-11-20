9633a0324e4ecf9fce3c42cb4473e7a111307f64
Benjamin Chen <yeehenchen@gmail.com>
2018-11-20 16:11:08 +0800

    added jet#create with currentUser(should be passed from MP as json

--- app/controllers/api/v1/jets_controller.rb
+++ app/controllers/api/v1/jets_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::JetsController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token
   before_action :find_jet, only: [:show]
 
   def index
@@ -8,9 +9,35 @@ class Api::V1::JetsController < Api::V1::BaseController
   def show
   end
 
+  def create
+    # pass current user in json format from MP
+    @user = User.find(user_params)
+    @jet = Jet.new(jet_params)
+    @jet.user = @user
+    if @jet.save
+      # return created object, for validation or added status (created 20X)
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   private
+
+  def jet_params
+    params.require(:jet).permit(:location, :model, :photo, :description, :capacity_of_passengers, :category, :manufactory, :user_id, :available_start_date, :available_end_date)
+  end
+
+  def user_params
+    params[:user][:id]
+  end
 
   def find_jet
     @jet = Jet.find(params[:id])
+  end
+
+  def render_error
+    render json: { errors: @jet.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
