09a51d29bcc7935994b9dafdc2eb7ccf1d8f9678
AveryJin <avery.jin@gmail.com>
2018-11-20 19:24:29 +0800

    update database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,49 +7,41 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 puts "Clearing Up DB"
-
 Review.delete_all
 Reservation.delete_all
 Jet.delete_all
 User.delete_all
 
 puts "Creating New DB for users"
-
 User.create(email: "test@gmail.com", name: "test")
 
-5.times do
-  User.create(email: Faker::Internet.email, name: Faker::Internet.username)
-end
-
-puts "Created #{User.count} users"
-
-puts "Creating New DB for Jets"
 available_start_date = '2018/11/23'
 available_end_date = '2020/11/23'
-price = ['15', '5', '6', '75'].sample
-location = ['SHA', 'PVG', 'NAY', 'ZUH'].sample
+price = ['15', '5', '6', '75', '50', '60', '18', '22', '44'].sample
+location = ['Beijing China Airport', 'Beijing Nanyuan Airport', 'Shenzhen Bao’an International', 'Zhuhai Jinwan Airport', 'Shenzhen Bao’an International', 'Hong Kong International', 'Shanghai Hongqiao International', 'Shanghai Pudong International', 'Hangzhou Xiaoshan International',
+'Lijian Sanyi Airport', ' Guangzhou Baiyun International', 'Macau International'].sample
 category = ['short-range', 'medium-rang', 'long-range'].sample
 model = ['Avro RJ70', 'Beechjet 400A', 'Premier 1'].sample
 manufactory = ['Boeing', 'Beechcraft', 'Cesna', 'Gulfstream'].sample
 capacity_of_passengers = ['4', '8', '16', '12'].sample
+rating = [1, 2, 3, 4, 5].sample
 
-7.times do
-  j = Jet.new(location: location, model: model, price_jet: price, category: category, manufactory: manufactory, capacity_of_passengers: capacity_of_passengers, available_start_date: available_start_date, available_end_date: available_end_date)
-  j.user = User.first
-  j.save
+10.times do
+  user = User.create(email: Faker::Internet.email, name: Faker::Internet.username)
+  jet = Jet.new(location: location, model: model, price_jet: price, category: category, manufactory: manufactory, capacity_of_passengers: capacity_of_passengers, available_start_date: available_start_date, available_end_date: available_end_date)
+  jet.user = user
+  jet.save
+  reservation = Reservation.new(customized_request: Faker::HarryPotter.quote, status: "true")
+  reservation.user = user
+  reservation.jet = jet
+  reservation.save
+  review = Review.new(content: Faker::Coffee.notes, rating: rating)
+  review.reservation = reservation
+  review.save
 end
-
+puts "Created #{User.count} users"
 puts "Created #{Jet.count} Jets"
-
-r = Reservation.new(customized_request: "test", status: "true")
-r.user = User.first
-r.jet = Jet.first
-r.save
-
 puts "Created #{Reservation.count} reservations"
-
-review = Review.new(content: "test", rating: 5)
-review.reservation = Reservation.first
-review.save
-
 puts "Created #{Review.count} Reviews"
+
+
