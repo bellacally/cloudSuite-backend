03b062cd740d35fe1f01fc4ca8b9892daaae0af5
Benjamin Chen <yeehenchen@gmail.com>
2018-11-23 10:37:42 +0800

    tested in post man should work

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,10 +4,12 @@ class Reservation < ApplicationRecord
   has_many :reviews
 
   def done?
-    if self.end_date < Time.now
-      self.status = true
-    else
-      self.status = false
+    if self.end_date.present?
+      if self.end_date < Time.now
+        self.status = true
+      else
+        self.status = false
+      end
     end
   end
 end
